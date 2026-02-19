77cf5272ec713b30d80391597cb8619567f20f7b
Dario Quental <dhdq@hotmail.com>
2026-02-19 18:30:05 +0000

    aula funçoes js

---
+++ JS/function.js
@@ -0,0 +1,20 @@
+//funcoes
+let total=0
+//objeto
+function  sayHello(){
+    console.log("Hello World");
+};
+
+sayHello()
+
+function soma( num1,num2){
+    let soma=0;
+    soma=num1+num2;
+    return soma;
+};
+
+let valor1=2
+let valor2=6
+
+total=soma(valor1,valor2)
+ console.log(total);
