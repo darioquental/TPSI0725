e4c26631defafc9e5abf352d429dbca9b25c0b00
Dario Quental <dhdq@hotmail.com>
2026-02-19 17:47:27 +0000

    operadores

---
+++ JS/operadores.js
@@ -0,0 +1,30 @@
+//operadores aritmeticos
+let valor1=12;
+let valor2=10;
+let soma;
+
+// = atribuiçao,  + soma, - subtração , / divisao , * multiplicaçao , % resto da divisao e ** exponencial
+soma=valor1+valor2;
+console.log(soma);
+console.log(valor1-valor2);
+console.log(valor1/valor2);
+console.log(valor1*valor2);
+console.log(valor1%valor2);
+console.log(valor1**valor2);
+
+// = atribuiçao , += , -= , *= , /=
+
+//soma+=valor ==  soma=soma+valor
+
+//Operadores de decisao
+// ==  se valores sao iguais
+// === se valores e tipo sao iguais 
+// !=  se valores sao diferentes        ex: 10 !=  "10"    false
+// !== se valores e tipo sao diferentes ex: 10 !== "10"   true
+
+// < , > , <= , >=
+
+//operadores Logicos
+//&&  and (e)
+//||  or (ou)
+// !  NOT ex: !true   -> false (not)
