56b63bac3e8ea5f1ce73d7d9c59f4fb9a40056bf
Dario Quental <dhdq@hotmail.com>
2026-04-06 20:01:56 +0100

    polish

--- 605/Aula 2/Hotel_Maravilha/Hotel_Maravilha/obj/Debug/net8.0/Hotel_Maravilha.AssemblyInfo.cs
+++ 605/Aula 2/Hotel_Maravilha/Hotel_Maravilha/obj/Debug/net8.0/Hotel_Maravilha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel_Maravilha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdadaf17327918f7d4a4a727141c04501307e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel_Maravilha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel_Maravilha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
